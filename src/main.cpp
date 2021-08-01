46123cf4985406059b47734d119ac6d89beb6664
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2021-08-01 11:46:00 +0200

    Code refactor + callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,99 @@
 #include <Arduino.h>
-
 #include <ArduinoJson.h>
-#include <Wire.h>
-
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
-
 #include "SailtrackModule.h"
+
+#define BATTERY_ADC_PIN 35
+#define BATTERY_ADC_MULTIPLIER 1.7
+#define CONNECTION_LED_PIN 5
 
 #define I2C_SDA 27
 #define I2C_SCL 25
 
-Adafruit_BNO055 bno;
+#define PUBLISH_RATE 10
+#define PUBLISH_PERIOD_MS 1000 / PUBLISH_RATE
 
-/* Set the delay between fresh samples */
-uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+Adafruit_BNO055 IMU;
 
-void setup()
-{
-	// Enable serial communication
-	Serial.begin(115200);
-
-	STModule.begin("imu", "sailtrack-imu", IPAddress(192,168,42,102));
-
-	Wire.begin(I2C_SDA, I2C_SCL);
-	bno = Adafruit_BNO055(55, 0x29, &Wire);
-
-	while (!bno.begin())
-	{
-		Serial.print(".");
+class ModuleCallbacks: public SailtrackModuleCallbacks {
+	void onWifiConnectionBegin() {
+		// TODO: Notify user
 	}
+	
+	void onWifiConnectionResult(wl_status_t status) {
+		// TODO: Notify user
+	}
+
+	DynamicJsonDocument getStatus() {
+		DynamicJsonDocument payload(300);
+		JsonObject battery = payload.createNestedObject("battery");
+		JsonObject cpu = payload.createNestedObject("cpu");
+		battery["voltage"] = analogRead(BATTERY_ADC_PIN) * BATTERY_ADC_MULTIPLIER / 1000;
+		cpu["temperature"] = temperatureRead();
+		return payload;
+	}
+};
+
+void beginIMU() {
+	Wire.begin(I2C_SDA, I2C_SCL);
+	IMU = Adafruit_BNO055(55, 0x29, &Wire);
+	IMU.begin();
 }
 
-void loop()
-{
-	//could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
-	sensors_event_t orientationData, angVelocityData, linearAccelData, magnetometerData, accelerometerData, gravityData;
-	bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-	bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
-	bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-	bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
-	bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-	bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+void setup() {
+	STModule.begin("imu", "sailtrack-imu", IPAddress(192,168,42,102));
+	STModule.setCallbacks(new ModuleCallbacks());
+	beginIMU();
+}
 
-	int8_t boardTemp = bno.getTemp();
+void loop() {
+	sensors_event_t eulerData, gyroData, linearAccelData, magnetometerData, accelerometerData, gravityData;
+	IMU.getEvent(&eulerData, Adafruit_BNO055::VECTOR_EULER);
+	IMU.getEvent(&gyroData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+	IMU.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+	IMU.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+	IMU.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+	IMU.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
 
-	uint8_t system, gyro, accel, mag = 0;
-	bno.getCalibration(&system, &gyro, &accel, &mag);
+	DynamicJsonDocument payload(300);
 
-	DynamicJsonDocument doc(300);
+	JsonObject euler = payload.createNestedObject("euler");
+	euler["x"] = eulerData.orientation.x;
+	euler["y"] = eulerData.orientation.y;
+	euler["z"] = eulerData.orientation.z;
 
-	JsonArray Orient = doc.createNestedArray("Orient");
-	Orient.add(orientationData.orientation.x);
-	Orient.add(orientationData.orientation.y);
-	Orient.add(orientationData.orientation.z);
+	JsonObject gyro = payload.createNestedObject("gyro");
+	gyro["x"] = gyroData.gyro.x;
+	gyro["y"] = gyroData.gyro.y;
+	gyro["z"] = gyroData.gyro.z;
 
-	JsonArray Rot = doc.createNestedArray("Rot");
-	Rot.add(angVelocityData.gyro.x);
-	Rot.add(angVelocityData.gyro.y);
-	Rot.add(angVelocityData.gyro.z);
+	JsonObject linearAccel = payload.createNestedObject("linear_accel");
+	linearAccel["x"] = linearAccelData.acceleration.x;
+	linearAccel["y"] = linearAccelData.acceleration.y;
+	linearAccel["z"] = linearAccelData.acceleration.z;
 
-	JsonArray Linear = doc.createNestedArray("Linear");
-	Linear.add(linearAccelData.acceleration.x);
-	Linear.add(linearAccelData.acceleration.y);
-	Linear.add(linearAccelData.acceleration.z);
+	JsonObject magnetometer = payload.createNestedObject("magnetometer");
+	magnetometer["x"] = magnetometerData.magnetic.x;
+	magnetometer["y"] = magnetometerData.magnetic.y;
+	magnetometer["z"] = magnetometerData.magnetic.z;
 
-	JsonArray Mag = doc.createNestedArray("Mag");
-	Mag.add(magnetometerData.magnetic.x);
-	Mag.add(magnetometerData.magnetic.y);
-	Mag.add(magnetometerData.magnetic.z);
-
-	JsonArray Accl = doc.createNestedArray("Accl");
-	Accl.add(accelerometerData.acceleration.x);
-	Accl.add(accelerometerData.acceleration.y);
-	Accl.add(accelerometerData.acceleration.z);
+	JsonObject accelerometer = payload.createNestedObject("accelerometer");
+	accelerometer["x"] = accelerometerData.acceleration.x;
+	accelerometer["y"] = accelerometerData.acceleration.y;
+	accelerometer["z"] = accelerometerData.acceleration.z;
 	
-	doc["temperature"] = boardTemp;
+	payload["temperature"] = IMU.getTemp();
 
-	JsonObject Calibration = doc.createNestedObject("Calibration");
-	Calibration["Sys"] = system;
-	Calibration["Gyro"] = gyro;
-	Calibration["Accel"] = accel;
-	Calibration["Mag"] = mag;
+	uint8_t calSystem, calGyro, calAccel, calMag = 0;
+	IMU.getCalibration(&calSystem, &calGyro, &calAccel, &calMag);
+	JsonObject calibration = payload.createNestedObject("calibration");
+	calibration["system"] = calSystem;
+	calibration["gyro"] = calGyro;
+	calibration["accel"] = calAccel;
+	calibration["mag"] = calMag;
 
-	STModule.publish("sensor/imu0", "imu0", doc);
+	STModule.publish("sensor/imu0", "imu0", payload);
 
-	delay(BNO055_SAMPLERATE_DELAY_MS);
+	delay(PUBLISH_PERIOD_MS);
 }
